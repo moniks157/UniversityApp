aac0145e5840d4a273a5f627496d186016779bd5
mmaczkowiak <monia.maczek@gmail.com>
2021-07-21 09:54:57 +0200

    added Database Migration

--- UniversityApp.DataAccess/Entities/Student.cs
+++ UniversityApp.DataAccess/Entities/Student.cs
@@ -8,7 +8,7 @@ namespace UniversityApp.DataAccess.Entities
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public int Age { get; set; }
-        //public bool IsAdult { get; set; }
+        public bool IsAdult { get; set; }
         public string Gender { get; set; }
         public virtual List<Grade> Grades { get; set; } = new List<Grade>();
     }
